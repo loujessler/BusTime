8386acf617842d49b84e3a0b6d9d0db889295002
loujessler <danilin055@yahoo.com>
2023-06-15 16:09:17 +0400

    add notification

--- data/messages/arrival_messages.py
+++ data/messages/arrival_messages.py
@@ -13,13 +13,15 @@ class ArrivalMessages:
     def bus_arrival_times(self):
         message = f'🚏 → {number_to_emoji(self.code_bus_stop)}\n\n'
         for arrival_time in self.arrival_times:
-            route_number = number_to_emoji(arrival_time['RouteNumber'])
+            route_number = int(arrival_time['RouteNumber'])
             minutes = arrival_time['ArrivalTime']
             sticker_time = '⚡️' if minutes <= 5 else '⏳'
+            type_transport = '🚌' if route_number < 400 else '🚐'
             line_msg = MessageFormatter(self.user.language).get_message({'arrival_bus_times': 'none'},
-                                                               {'route_number': route_number,
-                                                                'minutes': minutes,
-                                                                'sticker_time': sticker_time}),
+                                                                        {'route_number': number_to_emoji(route_number),
+                                                                         'type_transport': type_transport,
+                                                                         'minutes': minutes,
+                                                                         'sticker_time': sticker_time}),
             message += line_msg[0] + "\n"
         return fmt.text(message)
 
