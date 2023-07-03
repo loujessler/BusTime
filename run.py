32a4786177cc79682a68c40e156e0dbe3ee74ed3
loujessler <danilin055@yahoo.com>
2023-07-03 23:57:51 +0400

    delete aiohttp, fix html_name for test.py

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
-import asyncio
 from aiogram import executor
 from bot.handlers import dp
-from bot.loader import shutdown, bot, app
+from bot.loader import shutdown
 from bot.bot import on_startup
-
-from aiohttp import web
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup, on_shutdown=shutdown)
