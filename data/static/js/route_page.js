179fe924f6749d7eff60cbf7e9f18a32f1294373
loujessler <danilin055@yahoo.com>
2023-07-04 19:08:00 +0400

    try paste route_page.js into webapp

--- data/static/js/route_page.js
+++ data/static/js/route_page.js
@@ -2,20 +2,19 @@ var WebApp = window.Telegram.WebApp;
 var MainButton = WebApp.MainButton;
 var BackButton = WebApp.BackButton;
 
+MainButton.text("Back")
 MainButton.show();
 BackButton.show();
 
 MainButton.onClick(function() {
-  WebApp.showAlert("Хорошо, ты нажал на главную кнопку.");
+  WebApp.close();
 });
 WebApp.onEvent('mainButtonClicked', function() {
   /* also */
 });
 
 BackButton.onClick(function() {
-  WebApp.showAlert("Нет пути назад!");
-
-  BackButton.hide();
+  WebApp.close();
 });
 WebApp.onEvent('backButtonClicked', function() {
   /* also */
