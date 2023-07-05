3bbcfb1b488e86e354569918f3f5e9e12b6b4e54
loujessler <danilin055@yahoo.com>
2023-07-05 21:16:36 +0400

    test12

--- home_page/js/ttc/route_page.js
+++ home_page/js/ttc/route_page.js
@@ -1,10 +1,7 @@
 var WebApp = window.Telegram.WebApp;
 
-window.onload = function() {{
-    document.getElementById('mystop').addEventListener('click', function (event) {{
-        event.preventDefault();
-        var stopID = event.target.innerText;
-        WebApp.sendData(stopID)
-        console.log('Stop ID clicked: ' + stopID);
-    }});
+function handleClick(element) {{
+    event.preventDefault();
+    var stopID = element.innerText;
+    console.log('Stop ID clicked: ' + stopID);
 }}
