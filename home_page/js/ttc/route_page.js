9ac17f84302d9a4e69c38e014cdba27003e99833
loujessler <danilin055@yahoo.com>
2023-07-07 14:32:10 +0400

    bug fix route_page.js get text

--- home_page/js/ttc/route_page.js
+++ home_page/js/ttc/route_page.js
@@ -1,10 +1,15 @@
 var WebApp = window.Telegram.WebApp;
 
 WebApp.expand()
-function handleClick(element) {{
+function handleClick(element) {
     event.preventDefault();
-    var stopID = element.innerText;
-    console.log('Stop ID clicked: ' + stopID);
-    WebApp.sendData(stopID)
-    WebApp.close();
-}}
+    var spanElement = element.querySelector('span'); // get the span element
+    if(spanElement) { // check if the span element exists
+        var stopID = spanElement.innerText; // get the text from the span
+        console.log('Stop ID clicked: ' + stopID);
+        WebApp.sendData(stopID)
+        WebApp.close();
+    } else {
+        console.log('Span element not found');
+    }
+}
