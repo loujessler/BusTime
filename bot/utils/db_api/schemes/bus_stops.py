3cebadc39ce6e26ea3dc5715d3ce2262751a7a4e
loujessler <danilin055@yahoo.com>
2023-07-13 14:39:11 +0400

    fix bug with seq

--- bot/utils/db_api/schemes/bus_stops.py
+++ bot/utils/db_api/schemes/bus_stops.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, sql, Sequence
+from sqlalchemy import Column, String, Integer, sql
 from sqlalchemy.orm import relationship
 
 from bot.utils.db_api.db_gino import TimedBaseModel
@@ -8,9 +8,9 @@ from bot.utils.db_api.schemes.user_stops_association import user_stops_associati
 class BusStop(TimedBaseModel):
     __tablename__ = 'bus_stops'
 
-    id = Column(Integer, Sequence('id_seq'), autoincrement=True, primary_key=True, unique=True)
+    id = Column(Integer, autoincrement=True, primary_key=True, unique=True)
     name = Column(String(200))
-    id_stop = Column(Integer, Sequence('id_stop_seq'), primary_key=True)
+    id_stop = Column(Integer, primary_key=True)
 
     # Отношение "многие ко многим" с User через ассоциативную таблицу
     users = relationship('User', secondary=user_stops_association, back_populates='bus_stops')
